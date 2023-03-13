5c5c649dbab1fe2f03454d762abeb185d7c049ae
Eric Dube <eric_dube@hotmail.com>
2023-03-13 13:42:52 -0400

    Add project files.

---
+++ CoderFoundry_FizzBuzzMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoderFoundry_FizzBuzzMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
